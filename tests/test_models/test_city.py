280df9eb58f9fe1d684616aeb3516bf4bd523170
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-06-26 21:30:04 +0300

    adding cite test code from V1 AirBnB

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ """
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
@@ -22,3 +23,13 @@ class test_City(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+        
+    def test_attributes(self):
+        """Test the attributes of the City class."""
+        city = City()
+        self.assertEqual(city.state_id, "")
+        self.assertEqual(city.name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
