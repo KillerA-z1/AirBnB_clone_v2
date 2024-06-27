740c252c93b505fc10aedd4bc66789c5bc0001e3
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-06-27 03:21:35 +0300

    Hot fix 05

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-import os
+"""This module instantiates an instance of the Storage that will be used.
 
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
+The storage type is determined by the value of the environment variable 'HBNB_TYPE_STORAGE'.
+If the value is 'db', an instance of DBStorage is created.
+Otherwise, an instance of FileStorage is created.
 
-storage = DBStorage() if os.getenv(
-	'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
-"""A unique FileStorage/DBStorage instance for all models.
-
-This module is responsible for instantiating an object of either the FileStorage or DBStorage class,
-depending on the value of the 'HBNB_TYPE_STORAGE' environment variable. If the value is set to 'db',
-a DBStorage instance is created, otherwise a FileStorage instance is created.
-
-The storage object serves as a unique instance of either FileStorage or DBStorage, and is used to
-manage the storage and retrieval of objects in the application.
-
-The storage object is initialized by calling the reload() method, which loads all the objects from
-the storage system into memory.
+The storage is then reloaded to load any existing data.
 """
+
+from os import getenv
+
+storage_type = getenv('HBNB_TYPE_STORAGE')
+
+if storage_type == 'db':
+	from models.engine.db_storage import DBStorage
+	storage = DBStorage()
+else:
+	from models.engine.file_storage import FileStorage
+	storage = FileStorage()
+
 storage.reload()
